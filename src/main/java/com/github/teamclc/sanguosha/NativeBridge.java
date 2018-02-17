b56d3034f8a36a51b0d7f034e9c6b5c8b93c98ff
Charlie Jiang <cqjjjzr@126.com>
2018-02-18 00:49:45 +0800

    Add some test code.

--- src/main/java/com/github/teamclc/sanguosha/NativeBridge.java
+++ src/main/java/com/github/teamclc/sanguosha/NativeBridge.java
@@ -10,20 +10,23 @@ public class NativeBridge {
     public static native int sendPrivateMesaage(String message, long toQQ);
 
     public static void init() {
-
+        log("已启动。", 10, "测试");
     }
 
     public static void destroy() {
-
+        log("已关闭。", 10, "测试");
     }
 
     public static void openSetting() {
-
+        log("尝试打开设置。", 10, "测试");
     }
 
     public static boolean groupMessageReceived(String message, int subtype, int msgid, long fromQQ, long fromGroup) {
-
+        if (message.contains("sgstest")) {
+            sendGroupMessage("测试Sgs。", fromGroup);
+            sendPrivateMesaage("测试Sgs。", fromQQ);
+            return true;
+        }
+        return false;
     }
-
-
 }
